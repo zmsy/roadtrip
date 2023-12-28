77f3a4e1b4ad927b98089eeb4f0a833110904d1b
Zach Morrissey <zachmor05@gmail.com>
2023-12-28 13:21:03 -0800

    Remove invalid entries from cache

--- src/cache.ts
+++ src/cache.ts
@@ -55,3 +55,37 @@ export const writeCacheJson = async (
   const fileName = getCacheFilePath(slug, subfolder);
   await fs.writeFile(fileName, contents);
 };
+
+/**
+ * Remove any invalid cache entries that either are errors or have known
+ * invalid response codes.
+ */
+export const clearInvalidEntries = async () => {
+  /** Helper function to remove any invalid JSON in the cache. */
+  const removeInvalidEntries = async (folderPath: string) => {
+    const entries = (await fs.readdir(folderPath)).filter(
+      (x) => path.extname(x) === ".json"
+    );
+
+    for (const entry of entries) {
+      const entryPath = path.join(folderPath, entry);
+
+      try {
+        const content = await fs.readFile(entryPath);
+        const parsed = JSON.parse(content.toString());
+
+        // Remove the file if it's an invalid entry
+        if (parsed["message"] !== undefined || parsed["error"] !== undefined) {
+          await fs.unlink(entryPath);
+        }
+      } catch (error) {
+        console.error(
+          `Error reading JSON file ${entryPath}: ${JSON.stringify(error)}`
+        );
+      }
+    }
+  };
+
+  removeInvalidEntries(path.join(getCacheDir(), "osrm"));
+  removeInvalidEntries(path.join(getCacheDir(), "overpass"));
+};
